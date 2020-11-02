b604d1eaf10926f840033047615d4ad222eb9f30
David Imm <d.imm@lectra.com>
2020-11-02 10:56:45 +0100

    [chore] Upgrade Nuget dependencies [GitVersionTask]

--- BuildAsJenkins.bat
+++ BuildAsJenkins.bat
@@ -1,3 +1,7 @@
-for /f "delims=" %%F in ('dir %~dp0src\*.sln /b /o-n') do set filename=%%F
-dotnet build ".\src\%filename%" --configuration "Release" /t:pack /p:Version="3.2.2.831" /p:Platform="Any CPU" /flp:"v=diag;logfile=C:\Temp\Build.txt"
+for /f "delims=" %%F in ('dir %~dp0*.sln /b /o-n') do set filename=%%F
+del .git\gitversion_cache /Q
+.\BuildProcess\gitversion.exe /output buildserver
+.\BuildProcess\gitversion.exe /diag
+dotnet build ".\%filename%" --configuration "Release" /p:Platform="Any CPU"  /flp:"v=diag;logfile=C:\Temp\Build.txt"
+dotnet test ".\%filename%" --logger:Trx
 PAUSE
